8fdda092213ae87f1c548ecdde03f6750e09eb16
Simona <sima.simute1999@gmail.com>
2025-04-27 14:32:39 +0300

    dynamic rope

---
+++ Assets/Scripts/Rope.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Rope : MonoBehaviour
+{
+    public Transform candy;
+    public GameObject ropeJoint;
+    public float offset = 0.2f;
+    
+    public List<Transform> joints = new();
+    
+    void Start()
+    {
+        int jointCount =(int)(Vector3.Distance(transform.position, candy.position) / offset);
+        
+        Vector3 pos = transform.position;
+
+        for (int i = 0; i < jointCount; i++)
+        {
+            GameObject joint = Instantiate(ropeJoint, pos, Quaternion.identity);
+            pos = Vector3.Lerp(transform.position, candy.position, (float)i / jointCount);
+            
+            if(i == 0) 
+                joint.GetComponent<Joint2D>().connectedBody = GetComponent<Rigidbody2D>();
+            else 
+                joint.GetComponent<Joint2D>().connectedBody = joints[joints.Count - 1].GetComponent<Rigidbody2D>();
+            
+            joints.Add(joint.transform);
+        }
+        //candy.GetComponent<Joint2D>().connectedBody = joints[^1].GetComponent<Rigidbody2D>();
+        Joint2D candyJoint = candy.AddComponent<HingeJoint2D>();
+        candyJoint.connectedBody = joints[^1].GetComponent<Rigidbody2D>();
+    }
+
+    
+}
